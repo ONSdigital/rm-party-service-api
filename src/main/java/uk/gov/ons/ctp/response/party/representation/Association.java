ec0f3fcc546902fcad4fc55016675627bc7a4b19
Martin Humphrey <martin.humphrey@ons.gov.uk>
2017-08-15 11:10:52 +0000

    CTPA-1322 CAll Party Service for Parent and pre-enrolled respondents

---
+++ src/main/java/uk/gov/ons/ctp/response/party/representation/Association.java
@@ -0,0 +1,23 @@
+
+package uk.gov.ons.ctp.response.party.representation;
+
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Java class for Association.
+ *
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class Association {
+
+  private String partyId;
+  private List<Enrolment> enrolments;
+
+}
---
+++ src/main/java/uk/gov/ons/ctp/response/party/representation/BusinessSampleUnit.java
@@ -0,0 +1,46 @@
+
+package uk.gov.ons.ctp.response.party.representation;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Java class for BusinessSampleUnit
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class BusinessSampleUnit {
+
+  private String checkletter;
+  private String frosic92;
+  private String rusic92;
+  private String frosic2007;
+  private String rusic2007;
+  private int froempment;
+  private int frotover;
+  private String entref;
+  private String legalstatus;
+  private String name;
+  private String entrepmkr;
+  private String region;
+  private String birthdate;
+  private String entname1;
+  private String entname2;
+  private String entname3;
+  private String runame1;
+  private String runame2;
+  private String runame3;
+  private String ruref;
+  private String tradstyle1;
+  private String tradstyle2;
+  private String tradstyle3;
+  private String seltype;
+  private String inclexcl;
+  private int cellNo;
+  private String formtype;
+  private String currency;
+
+}
