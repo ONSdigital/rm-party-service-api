c3602eb7fe3d244c8b4cf5360c299f9f7e279e44
sdiaz1000 <sdiaz1000@gmail.com>
2017-08-15 14:40:47 +0100

    Re order Attributes

--- src/main/java/uk/gov/ons/ctp/response/party/representation/Attributes.java
+++ src/main/java/uk/gov/ons/ctp/response/party/representation/Attributes.java
@@ -9,32 +9,31 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class Attributes {
+  public String entrepmkr;
   public String birthdate;
-  public long cellNo;
   public String checkletter;
-  public String currency;
+  public String rusic2007;
   public String entname1;
   public String entname2;
   public String entname3;
-  public String entref;
-  public String entrepmkr;
-  public String formtype;
-  public long froempment;
-  public String frosic2007;
-  public String frosic92;
-  public long frotover;
-  public String inclexcl;
-  public String legalstatus;
-  public String name;
-  public String region;
   public String runame1;
   public String runame2;
   public String runame3;
-  public String ruref;
-  public String rusic2007;
+  public String frosic2007;
+  public String frosic92;
   public String rusic92;
+  public String inclexcl;
+  public long froempment;
+  public long cellNo;
+  public String formtype;
+  public String legalstatus;
+  public String entref;
+  public long frotover;
   public String seltype;
+  public String currency;
+  public String region;
   public String tradstyle1;
   public String tradstyle2;
   public String tradstyle3;
+  public String name;
 }
