1ca7e4028557ee303d526ae20a5fb69aebde6b69
Philippe Brossier <brossierp@gmail.com>
2017-09-08 09:21:39 +0100

    CTPA-1557 adding missing field in party svc api

--- src/main/java/uk/gov/ons/ctp/response/party/representation/PartyDTO.java
+++ src/main/java/uk/gov/ons/ctp/response/party/representation/PartyDTO.java
@@ -5,12 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class PartyDTO {
-  private  String id;
+  private String id;
   private String sampleUnitType;
   private String sampleUnitRef;
   private Attributes attributes;
+  private List<Association> associations;
 }
