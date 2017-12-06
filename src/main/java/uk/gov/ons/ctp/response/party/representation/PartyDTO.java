cfdf379144ee87126fc6bcd2882655d66baa9c50
Richard Weeks <richard.weeks@live.co.uk>
2017-12-06 12:24:53 +0000

    Add Sample Summary ID. (#2)
    * Add Sample Summary ID.

--- src/main/java/uk/gov/ons/ctp/response/party/representation/PartyDTO.java
+++ src/main/java/uk/gov/ons/ctp/response/party/representation/PartyDTO.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class PartyDTO {
   private String id;
   private String sampleUnitType;
+  private String sampleSummaryId;
   private String sampleUnitRef;
   private Attributes attributes;
   private List<Association> associations;
